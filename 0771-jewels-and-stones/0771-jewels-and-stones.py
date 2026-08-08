2bf269eb55ae41f5454c38c0159a6c5fa3a73e07
Dheena Dhyalan C <dheenadhyalan07@gmail.com>
2026-08-08 22:41:23 +0530

    Time: 0 ms (100%), Space: 19.2 MB (53.65%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,3 +1,9 @@
-class Solution:
-    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        res = 0
+        for stone in stones:
+            for jewel in jewels:
+                if jewel == stone:
+                    res += 1
+        return res
         
